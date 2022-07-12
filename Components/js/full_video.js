ddd36b9d43133f28b231a6fb632597e87dd4aa23
veterdelay <alxrorel0100@gmail.com>
2022-07-12 21:28:17 +0300

    git new commit

--- Components/js/full_video.js
+++
@@ -1,7 +0,0 @@
-$('.full_video_block').click(function(){
-    $('.pre-video').each(function(){
-        $(this).css('display','none');
-    });
-    $('.video-iframe').css('display','block');
-    $('.video-iframe')[0].src += "?autoplay=1";
-});
